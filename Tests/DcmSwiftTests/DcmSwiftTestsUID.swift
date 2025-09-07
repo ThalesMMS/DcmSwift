7d4a50e35e02d0106ae0729d7d03e68c57610399
Thales Matheus <133025183+ThalesMMS@users.noreply.github.com>
2025-09-07 04:02:41 -0300

    Fix UID validation for zero components

--- Tests/DcmSwiftTests/DcmSwiftTestsUID.swift
+++ Tests/DcmSwiftTests/DcmSwiftTestsUID.swift
@@ -15,10 +15,14 @@ class DcmSwiftTestsUID: XCTestCase {
     public func testValidateUID() {
         let valUID = "1.102.99"
         XCTAssertTrue(UID.validate(uid: valUID))
-        
+
+        // component "0" should be considered valid
+        let valUIDWithZero = "1.2.840.10008.0.1.1"
+        XCTAssertTrue(UID.validate(uid: valUIDWithZero))
+
         let invalUID1 = "1.012.5"
         let invalUID2 = "coucou"
-        
+
         XCTAssertFalse(UID.validate(uid: invalUID1))
         XCTAssertFalse(UID.validate(uid: invalUID2))
     }
